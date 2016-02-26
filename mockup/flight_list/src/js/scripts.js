83d8af2476f54f818a092f8d2726f6c9abcc80ef
kevin youna <kevin.youna@gmail.com>
2016-02-26 03:27:54 +0100

    In the flight list page the selected criterias are showed. For now , It won't do anything except graphic things

--- mockup/flight_list/src/js/scripts.js
+++ mockup/flight_list/src/js/scripts.js
@@ -1,1 +1,41 @@
-// Empty JS for your own code to be here
+$(document).ready(function(){
+	
+
+	function sayHello(){
+		console.log('clicked');
+	}
+
+	function function_ajout_critere(x){
+		console.log('x.value '+ x.value);
+
+		var element = document.createElement("button");
+		element.setAttribute("id",'name_'+x.value);
+		element.setAttribute("class","btn btn-success");
+		element.innerHTML = x.value;
+		var foo = document.getElementById("criteria");
+		foo.appendChild(element);
+		foo.element.value = x.value;
+	}
+
+
+	console.log("ready");
+
+	var number = document.getElementById("number");
+	var atc = document.getElementById("atc");
+	var departure_time = document.getElementById("departure_time");
+	var arrival_time = document.getElementById("arrival_time");
+	var departure_date = document.getElementById("departure_date");
+	var arrival_date = document.getElementById("arrival_date");
+	var departure_airport = document.getElementById("departure_airport");
+	var arrival_airport = document.getElementById("arrival_airport");
+	var e = document.getElementById("option_selection");
+	console.log(e.value);
+	console.log('e '+e);
+	option_selection.addEventListener("change",function(){
+		function_ajout_critere(e);
+	});
+
+	
+	
+
+});
--- mockup/flight_list_cco_criteria/src/js/scripts.js
+++ mockup/flight_list_cco_criteria/src/js/scripts.js
@@ -1,1 +1,13 @@
-// Empty JS for your own code to be here
+$(document).ready(function(){
+	console.log("ready");
+	//alert("hello world");
+	function sayHello(){
+		console.log('clicked');
+	}
+	var btn_create_flight = document.getElementById("Button_create_flight");
+	btn_create_flight.addEventListener("click",function(){
+		sayHello();
+	});
+
+});
+
