9dc83302830ae2d8a42e29363d547e158ddf7933
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-27 11:47:51 +0100

    Refactored the FlightDAO

--- workspace/Flight/src/main/java/com/g4/dao/FlightDao.java
+++ workspace/Flight/src/main/java/com/g4/dao/FlightDao.java
@@ -8,8 +8,8 @@ import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
 
-import com.g4.dao.datanucleus.Airport;
-import com.g4.dao.datanucleus.Flight;
+import dao.datanucleus.Airport;
+import dao.datanucleus.Flight;
 
 public class FlightDao {
 
@@ -18,16 +18,42 @@ public class FlightDao {
 			String depart_date, String arriv_date, 
 			String depart_hour, String arriv_hour){
 		
+		Object airportID1 = null;
+		Object airportID2 = null;
 		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("FlightGL");
 		PersistenceManager pm = pmf.getPersistenceManager();
 		Transaction tx = pm.currentTransaction();
 		
+		// Get ids
+		try{
+			airportID1 = JDOHelper.getObjectId(depart_airport);
+			airportID2 = JDOHelper.getObjectId(arriv_airport);
+			
+		}catch(Exception e){
+			System.out.print("Error while adding the flight " + e.getMessage());
+			throw e;
+		}finally{
+			
+			if(tx.isActive()){
+				
+				tx.rollback();
+			}
+			pm.close();
+		}
+		
+		// Update the persistence Manager
+		pm = pmf.getPersistenceManager();
+		tx = pm.currentTransaction();
+		
+		// Relationship between the flight and the airports
 		try{
 			
 			tx.begin();
-			Flight fl = new Flight(cm, atc,depart_airport,arriv_airport,
-									depart_date,arriv_date,
-									depart_hour,arriv_hour);
+			Airport a1 = (Airport)pm.getObjectById(airportID1);
+			Airport a2 = (Airport)pm.getObjectById(airportID2);
+			
+			Flight fl = new Flight(cm, atc,a1,a2,depart_date,
+									arriv_date,depart_hour,arriv_hour);
 			pm.makePersistent(fl);
 			tx.commit();
 			
