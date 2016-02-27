00389a847b9cee78316b9c01000300a6d9a1af90
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-27 12:30:14 +0100

    Added a internal class into User. It is necessary to create the multiple association between Flight and User through the association table

--- workspace/Flight/src/main/java/com/g4/dao/datanucleus/User.java
+++ workspace/Flight/src/main/java/com/g4/dao/datanucleus/User.java
@@ -1,5 +1,8 @@
 package com.g4.dao.datanucleus;
 
+import java.io.Serializable;
+import java.util.StringTokenizer;
+
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 import javax.jdo.annotations.Unique;
@@ -53,5 +56,58 @@ public class User {
 	public String getType(){
 		
 		return user_type;
+	}
+	
+	public static class UserPK implements Serializable{
+		
+		private static final long serialVersionUID = 1L;
+		int user_id;
+		
+		
+		public UserPK(){
+			
+		}
+		
+	    public UserPK(String value) {
+	    	
+	        StringTokenizer token = new StringTokenizer (value, "::");
+	        token.nextToken();
+	        
+	        try{
+	        	
+	        	this.user_id = Integer.parseInt(token.nextToken());
+	        
+	        }catch(NumberFormatException ne){
+	        	
+	        	ne.printStackTrace();
+	        }
+	    }
+
+	    public boolean equals(Object obj)
+	    {
+	        if (obj == this){
+	            return true;
+	        }
+	        
+	        if (!(obj instanceof UserPK)){
+	            return false;
+	        }
+	        
+	        UserPK c = (UserPK)obj;
+
+	        return this.user_id == c.user_id;
+	    }
+
+	    public int hashCode ()
+	    {
+	        return Integer.hashCode(user_id);
+	    }
+
+	    public String toString ()
+	    {
+	        // Give output expected by String constructor
+	        return this.getClass().getName() + "::"  + this.user_id;
+	    }
+		
 	}
 }
