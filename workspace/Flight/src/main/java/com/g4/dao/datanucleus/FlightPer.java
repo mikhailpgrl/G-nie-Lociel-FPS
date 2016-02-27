3257edc20971fcf19fbcd022ddc5a7ceaa000263
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-27 12:47:48 +0100

    Added the User/Flight table association

---
+++ workspace/Flight/src/main/java/com/g4/dao/datanucleus/FlightPer.java
@@ -0,0 +1,80 @@
+package com.g4.dao.datanucleus;
+
+import java.io.Serializable;
+import java.util.StringTokenizer;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class FlightPer {
+
+	@PrimaryKey
+	Flight flight;
+
+	@PrimaryKey
+	User user;
+	
+	protected FlightPer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public FlightPer(Flight f, User u){
+		
+		flight = f;
+		user = u;
+		
+	}
+	
+	public Flight getFlight(){
+		
+		return flight;
+	}
+	
+	public User getUser(){
+		
+		return user;
+	}
+	
+	
+	public static class PK implements Serializable{
+		
+		private static final long serialVersionUID = 1L;
+		Flight.FlightPK flight;
+		User.UserPK user;
+		
+		protected PK() {
+			
+		}
+		
+		public PK(String string){
+			
+			StringTokenizer token = new StringTokenizer(string,"--");
+			token.nextToken();
+			this.flight = new Flight.FlightPK(token.nextToken());
+			this.user = new User.UserPK(token.nextToken());
+			
+		}
+
+        public String toString()
+        {
+            return (flight.toString() + "--" + user.toString());
+        }
+
+        public int hashCode()
+        {
+            return flight.hashCode() ^ user.hashCode();
+        }
+
+        public boolean equals(Object other)
+        {
+            if (other != null && (other instanceof PK))
+            {
+                PK otherPK = (PK)other;
+                return this.flight.equals(otherPK.flight) && this.user.equals(otherPK.user);
+            }
+            return false;
+        }
+	}
+	
+}
