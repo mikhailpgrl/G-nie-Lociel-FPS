abd4052a58b64f73e12e3e494f1ba466e2df7c7e
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-27 11:29:12 +0100

    Refactored the Flight. It was not good

--- workspace/Flight/src/main/java/com/g4/dao/datanucleus/Flight.java
+++ workspace/Flight/src/main/java/com/g4/dao/datanucleus/Flight.java
@@ -1,11 +1,12 @@
 package com.g4.dao.datanucleus;
 
+import java.util.StringTokenizer;
 
 import javax.jdo.annotations.ForeignKey;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 
-@PersistenceCapable(objectIdClass=FlightPK.class)
+@PersistenceCapable(objectIdClass=Flight.FlightPK.class)
 public class Flight {
 
 	@PrimaryKey
@@ -62,6 +63,7 @@ public class Flight {
 		return arrival_airport.getICAO();
 	}
 	
+	
 	public String getDepartureDate(){
 		
 			return departure_date;
@@ -82,6 +84,68 @@ public class Flight {
 		return arrival_hour;
 	}
 	
+	// Internal class for composite primary key
+	public static class FlightPK{
+		
+		String commercial_number;
+		int departure_airport;
+		String departure_date;
+		
+		
+		public FlightPK(){
+			
+		}
+		
+	    public FlightPK(String value) {
+	    	
+	        StringTokenizer token = new StringTokenizer (value, "::");
+	        token.nextToken();
+	        this.commercial_number = token.nextToken();
+	        
+	        try{
+	        	
+	        	this.departure_airport = Integer.parseInt(token.nextToken());
+	        
+	        }catch(NumberFormatException ne){
+	        	
+	        	ne.printStackTrace();
+	        }
+	        
+	        this.departure_date = token.nextToken();	
+	    }
+
+	    public boolean equals(Object obj)
+	    {
+	        if (obj == this){
+	            return true;
+	        }
+	        
+	        if (!(obj instanceof FlightPK)){
+	            return false;
+	        }
+	        
+	        FlightPK c = (FlightPK)obj;
+
+	        return commercial_number.equals(c.commercial_number) 
+	        		&& departure_airport == c.departure_airport
+	        		&& departure_date.equals(c.departure_date);
+	    }
+
+	    public int hashCode ()
+	    {
+	        return this.commercial_number.hashCode() 
+	        		^ this.departure_airport 
+	        		^ this.departure_date.hashCode();
+	    }
+
+	    public String toString ()
+	    {
+	        // Give output expected by String constructor
+	        return this.getClass().getName() + "::"  + this.commercial_number 
+	        		+ "::" + this.departure_airport + "::" + this.departure_airport;
+	    }
+		
+	}
 }
 
 
